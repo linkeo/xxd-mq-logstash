5f1d1e99dc73011011532f5f71844a1db4f8d273
林康 <linkang@innobuddy.com>
2018-09-25 10:38:22 +0800

    feat: 去掉无用的依赖

--- example.js
+++ example.js
@@ -1,2 +1,16 @@
-'use strict';
+const MQ = require('./index'); // require('xxd-mq-logstash');
 
+const queue = new MQ({
+  server: 'amqp://user:pass@127.0.0.1/path',
+  queue: 'some:queue',
+
+  // define queue global payload
+  payload: {
+    project: 'example-project',
+  },
+});
+
+queue.push({
+  message: 'Hello world',
+  // Auto append @timestamp
+});
